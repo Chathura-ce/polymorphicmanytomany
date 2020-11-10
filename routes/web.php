00493a9e5cee3a046d49e36910e7492b29566658
chathura-ce <chathu.eac@gmail.com>
2020-11-10 07:09:10 +0530

    create data

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    $post = Post::create(['name' => 'my first post']);
+
+    $tag1 = Tag::find(1);
+
+
+    $post->tags()->save($tag1);
+
+    $video = Video::create(['name' => 'video.mkv']);
+
+
+    $tag2 = Tag::find(2);
+
+    $video->tags()->save($tag2);
 });
